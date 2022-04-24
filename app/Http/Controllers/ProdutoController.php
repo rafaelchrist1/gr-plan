0fe8d86a8b5645dd4177e53fcc31107fc6175332
rafaelchrist1 <rafael.christ1997@gmail.com>
2022-04-24 01:03:05 -0300

    swagger doc api

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -10,6 +10,28 @@ use Illuminate\Support\Facades\Validator;
 
 class ProdutoController extends Controller
 {
+
+    /**
+     * @OA\Get(
+     *      path="/api/produto",
+     *      operationId="index",
+     *      tags={"Produtos"},
+     *      summary="Get list of produtos",
+     *      description="Returns list of produtos",
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *       ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      )
+     *     )
+     */
     public function index()
     {
         $produtos = Produto::with('marca')->get()->toArray();
@@ -17,12 +39,42 @@ class ProdutoController extends Controller
 
         return array_reverse($produtos);
     }
+
+
     public function create()
     {
         $marcas = Marca::all();
         return response()->json($marcas);
     }
 
+    /**
+     * @OA\Post(
+     *      path="/api/produto",
+     *      operationId="produto.store",
+     *      tags={"Produtos"},
+     *      summary="Store new project",
+     *      description="Returns project data",
+     *      * @OA\RequestBody(
+     *    required=true,
+     *    description="Pass user credentials",
+     *    @OA\JsonContent(
+     *       required={"nome"},
+     *       @OA\Property(property="nome", type="string", example="Freezer"),
+     *       @OA\Property(property="descricao", type="string", example="texto longo"),
+     *       @OA\Property(property="tensao", type="string", example="220v"),
+     *       @OA\Property(property="marca_id", type="integer", example="1"),
+     *    ),
+     * ),
+     *      @OA\Response(
+     *          response=201,
+     *          description="Successful operation",
+     *       ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad Request"
+     *      )
+     * )
+     */
     public function store(Request $request)
     {
 
@@ -38,7 +90,7 @@ class ProdutoController extends Controller
         };
 
         Produto::create($request->all());
-        return response()->json('Eletodoméstico criado!');
+        return response()->json('Eletrodoméstico criado!');
     }
 
     public function show($id)
@@ -60,12 +112,12 @@ class ProdutoController extends Controller
             return response()->json($validator->messages(), 400);
         };
         $produto->update($request->all());
-        return response()->json('Eletodoméstico atualizado!');
+        return response()->json('Eletrodoméstico atualizado!');
     }
     public function destroy($id)
     {
         $produto = Produto::find($id);
         $produto->delete();
-        return response()->json("Eletodoméstico <" . $produto->nome . "> removido!");
+        return response()->json("Eletrodoméstico <" . $produto->nome . "> removido!");
     }
 }
